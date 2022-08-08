40595818bf1792e05ed080a1794d66ce8d303c7a
Flutter PC <sina.sam@gmail.com>
2022-08-08 18:11:47 +0430

    last

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Footer from "./Footer";
 import Hearder from "./Hearder";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
 import CreateArea from "./CreateArea";
 
 
 function App() {
-  return (
+  const [notes, setNotes] = useState([]);
+  
+
+  function addNote(note) {
+    
+    //  var newNotes = notes.push(note);
+
+    setNotes(prevValue => {
+      return [...prevValue, note]; 
+    });
+  }
+ 
+  function delelteNote(id) {
+      setNotes(prevValue => {
+        prevValue.filter((note, index)=> {
+            return index !== id;
+        })
+      })
+
+  }
+
+
+  
+  return ( 
     
     <div className="App">
     
     <Hearder />
-    <CreateArea />
+    <CreateArea onAdd={addNote} />
     
-      {notes.map ((note) =>    
+      {notes.map ((note, index) =>    
         <Note 
-          key = {note.key}
+          key = {index}
+          id = {index}
           title = {note.title}
           content = {note.content}
+          onDelete = {delelteNote}
         />
       )}
     <Footer />
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+
+
+
+
+
+
+function CreateArea(props) {
+   
+
+    const [ value, setValue] = useState({
+        title: "",
+        content: ""
+        })
+
+    function handleChange(event) {
+        const newValue = event.target.value;
+        const name = event.target.name;        
+
+        setValue((prevValue) => {
+            if (name==="title") {
+                return {
+                    title: newValue,
+                    content: prevValue.content
+                        };
+            } else if (name==="content") {
+                return {
+                    title: prevValue.title,
+                    content: newValue
+                        };
+            }
+            
+        })
+    }
+
+    function handleClick(event) {
+        
+        
+        props.onAdd(value);
+        event.preventDefault();
+
+        
+    
+        
+    }
+   
+    
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+     
+        <input name="title" placeholder="Title" onChange={handleChange}  />
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange}  />
+        <button onClick={handleClick}>Add</button>
       </form>
     </div>
   );
